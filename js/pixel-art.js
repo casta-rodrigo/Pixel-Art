1f1f12e38d475b2b58503e43123a5af90856b556
casta <rocasta500@gmail.com>
2018-06-19 20:10:25 -0300

    v5

--- js/pixel-art.js
+++ js/pixel-art.js
@@ -29,10 +29,11 @@ function see_array(nombreColores){
     div_create.attributes = 'backgroundColor';
     div_create.style.backgroundColor = nombreColores[i];
     div_create.className = 'color-paleta';
-    paleta.appendChild(div_create);
+    palet.appendChild(div_create);
   }
 }
 see_array(nombreColores);
+
 
 var div_pixel = document.createElement('div');
 for(i = 0; i < 1750; i++){
@@ -40,18 +41,21 @@ for(i = 0; i < 1750; i++){
   list_pixels.appendChild(div_pixel);
 }
 
-var selected_color = document.getElementById('indicador-de-color');
 
-var div_colors = document.querySelector('#paleta');
-
-div_colors.addEventListener('click', change_color);
-
+var selected_color = document.getElementById('indicador-de-color'); 
+palet.addEventListener('mousedown', change_color);
 function change_color(e){
-  div_colors = e.target.style.backgroundColor;
-  selected_color.style.backgroundColor = div_colors;
+  selected_color.style.backgroundColor = e.target.style.backgroundColor;
 }
 
 
+
+list_pixels.addEventListener('mousedown',painting);
+function painting (e){
+  e.target.style.backgroundColor = selected_color.style.backgroundColor;
+  //div_colors.style.backgroundColor = selected_color;
+  console.log(list_pixels);
+}
 
 // Variable para guardar el elemento 'color-personalizado'
 // Es decir, el que se elige con la rueda de color.
@@ -66,3 +70,8 @@ colorPersonalizado.addEventListener('change',
 
   })
 );
+
+
+
+
+
